c97d2e2006dd85c92d883872618ff28a1b630a2f
lepshiBel <eliseyavvakumov@mail.ru>
2023-11-23 12:17:44 +0100

    Added categories menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const SCREEN_WIDTH = window.screen.width;
+var isOpen = false;
+
+function changeGlobalVar(value) {
+    isOpen = value;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open");
+        
+        var h = document.getElementById("menu").clientHeight;
+  
+        if (isOpen === false) {
+            document.getElementById("main-container").style.marginTop = h - 200 + "px";      
+            changeGlobalVar(true);
+        } 
+        else {
+            document.getElementById("main-container").style.marginTop = h - 10 + "px";
+            changeGlobalVar(false);
+        }
     });
 });
